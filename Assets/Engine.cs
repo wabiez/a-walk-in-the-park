306b271d390ab56d4392c4249d6c6f3a2c5b14b9
Fayez Barbari <fayez.barbari@grainbridge.com>
2020-04-19 18:37:40 -0500

    init

---
+++ Assets/Engine.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Engine : MonoBehaviour
+{
+    [SerializeField] Text textComponent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textComponent.text = ("carfull! you might die of beaties");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
